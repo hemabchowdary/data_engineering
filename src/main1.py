509f4301b3462ad3a8bd284b6424d71715a08c5d
Hema <hemabchowdary95@gmail.com>
2024-10-08 07:15:30 -0500

    chnages were done

--- src/main1.py
+++ src/main1.py
@@ -1,8 +1,10 @@
-age = 28
-height = 5.4
-color = "green"
+age=28 
+height=5.6 
+color="Green" 
 like_python = True
-print(age)
-print(height)
-print(color)
-print(like_python)
+print("age", "height", "color","like_python")
+page=40 
+height=5.8
+color="Blue"
+like_python = False
+print("age", "height", "color","like_python")
