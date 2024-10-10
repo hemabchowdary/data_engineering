07723ee6be598d0d48df5f0f50cadf2068a9f7c8
Hema <123908919+hemabchowdary@users.noreply.github.com>
2024-10-10 07:42:15 -0500

    Delete src/assignmet1.py

--- src/assignmet1.py
+++
@@ -1,8 +0,0 @@
-l = '''this is a basic python program 
-and i would like to learn more this programming language
-as soon as possible'''
-k = l.split()
-print(k)
-' '.join(k[:5])
-string = "welcome to my data engineering"
-print(len(string))
