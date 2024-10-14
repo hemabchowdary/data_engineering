1e3838d77cc62b6bb4e96e3925106c511bcf60e2
Hema <hemabchowdary95@gmail.com>
2024-10-14 08:56:42 -0500

    chnages are done

---
+++ src/assignment10.py
@@ -0,0 +1,22 @@
+num = int(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+# Taking score input and checking its range
+score = int(input("Enter your score: "))
+
+if score > 90:
+    print("The score is greater than 90.")
+elif 80 <= score <= 89:
+    print("The score is between 80 and 89.")
+elif 70 <= score <= 79:
+    print("The score is 70 or between 71 and 79.")
+else:
+    print("The score is less than 70.")
+    
