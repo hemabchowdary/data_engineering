0aedbecae689ef524e931f0c24514af1652e9b23
Hema <hemabchowdary95@gmail.com>
2024-10-17 08:12:46 -0500

    chnage are done

--- src/factorial.py
+++ src/factorial.py
@@ -1,4 +1,3 @@
-# Function to calculate factorial
 def factorial(n):
     if n == 0 or n == 1:  # Base case: factorial of 0 or 1 is 1
         return 1
@@ -7,4 +6,6 @@ def factorial(n):
 
 # Example usage
 num = int(input("Enter a number to calculate its factorial: "))
+print(f"Factorial of {num} is {factorial(num)}")
+
 
